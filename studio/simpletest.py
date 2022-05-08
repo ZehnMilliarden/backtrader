d473d543286fe24f4e52d4d5ff8661407b3ce958
ZehnMilliarden <git@lxf.science>
2022-05-08 20:57:57 +0800

    【studio】【.vscode】新增调试选项，和默认调试策略调整

--- studio/simpletest.py
+++ studio/simpletest.py
@@ -2,6 +2,7 @@
 # https://blog.csdn.net/h00cker/category_11448348.html
 
 from Strategy.test_manager import TestManager
+from Strategy.test_strategy_b import TestStrategyPlanB
 
 if __name__ == '__main__':
-    TestManager.run("TestStrategyDefault")
+    TestManager.run(TestStrategyPlanB.__name__)
