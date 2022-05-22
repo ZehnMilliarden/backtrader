56df1d3b24578288a34bf4fc4d619ac3afde5ba3
ZehnMilliarden <git@lxf.science>
2022-05-22 23:42:50 +0800

    【Strategy】更新测试流程

--- studio/Strategy/test_strategy_c.py
+++ studio/Strategy/test_strategy_c.py
@@ -1,6 +1,7 @@
 
 from pandas import Period
 import backtrader
+from backtrader.indicators import sma
 from .default_strategy import TestStrategyDefault
 
 
@@ -17,6 +18,14 @@ class TestStrategyPlanC(TestStrategyDefault):
 
         self.sma = backtrader.indicators.SMA(
             self.datas[0], period=self.params.period)
+
+        close_over_sma = self.dataclose > self.sma
+
+        self.sma_dist_to_high = self.data.high - self.sma
+
+        sma_dist_small = self.sma_dist_to_high < 3.5
+
+        self.sell_sig = backtrader.And(close_over_sma, sma_dist_small)
 
     def test_data(self):
 
@@ -36,8 +45,22 @@ class TestStrategyPlanC(TestStrategyDefault):
         ))
 
         # 获取 -4 到 0 的切片数据, x-y+1 -> x 的数据
-        data0Slice = self.data.get(0, 5)
-        print(data0Slice)
+        data0_Slice = self.data.get(0, 5)
+        print(data0_Slice)
+
+    def log_v1(self):
+        if self.sma > 30.0:
+            print('sma %.2f is greater than 30.0' % self.sma[0])
+        if self.sma > self.data.close:
+            print('sma %.2f is above the close price %.2f' %
+                  (self.sma[0], self.dataclose[0]))
+        if self.sell_sig:  # if sell_sig == True: 这种写法也可以
+            print('sell sig is True')
+        else:
+            print('sell sig is False')
+        if self.sma_dist_to_high > 5.0:
+            print('distance from sma to hig is greater than 5.0')
 
     def next(self):
         self.test_data()
+        self.log_v1()
