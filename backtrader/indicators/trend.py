a39d89ba0d516c69bafb2956ba88f5e6697b54e7
lxf <git@lxf.sience>
2024-08-03 07:16:44 +0800

    【trend.py】画线逻辑调整

--- backtrader/indicators/trend.py
+++ backtrader/indicators/trend.py
@@ -11,25 +11,40 @@ class Trend(Indicator):
     alias = ('Trend', )
     params = {'period_fast': 10, 'period_slow': 20}
     plotinfo = dict(subplot=False)
-    lines = ('fast', 'slow', 'trend',)
+    lines = ('fast', 'slow', 'trend_0', 'trend_1',)
 
     def __init__(self):
         self.lines.fast = Average(self.data, period=self.params.period_fast)
         self.lines.slow = Average(self.data, period=self.params.period_slow)
         self.cross = CrossOver(
             self.lines.fast, self.lines.slow)
-        self.dst = self.lines.trend.array
         super(Trend, self).__init__()
 
     def next(self):
-        self.lines.trend.array[0] = self.data[0]
+        self.lines.trend_0[0] = self.data[0]
+        self.lines.trend_1[0] = self.data[0]
 
     def once(self, start, end):
+        currentLine = self.lines.trend_0
+        preLine = self.lines.trend_1
         for index, crs in enumerate(self.cross):
             if index in range(start, end):
                 if math.isnan(crs):
-                    self.lines.trend[index] = 0.0
+                    self.lines.trend_0[index] = crs
+                    self.lines.trend_1[index] = crs
                 elif crs != 0:
-                    self.lines.trend[index] = self.data[index]
+                    if currentLine is self.lines.trend_0:
+                        currentLine = self.lines.trend_1
+                        preLine = self.lines.trend_0
+                    else:
+                        currentLine = self.lines.trend_0
+                        preLine = self.lines.trend_1
+                    currentLine[index] = self.data[index]
                 else:
-                    self.lines.trend[index] = self.lines.trend[index - 1]
+                    if math.isnan(currentLine[index-1]):
+                        currentLine[index] = preLine[index-1]
+                    else:
+                        currentLine[index] = currentLine[index-1]
+
+    def _ConvexHull(self):
+        pass
